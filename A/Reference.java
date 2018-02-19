723ff41789ad8dcaf72dad812123b08ab322552c
usuario <usuario@161.67.196.140>
2018-02-19 13:15:10 +0100

    primeros pasos

--- A/Reference.java
+++ A/Reference.java
@@ -4,5 +4,9 @@ package concurrent_assignment1.A;
  * Get the reference of the main thread */
 
 public class Reference {
-
+    Thread h = Thread.currentThread ();
+    int i;
+    for(i=1;i<100;i=i+2){
+        System.out.println(i);
+    }
 }

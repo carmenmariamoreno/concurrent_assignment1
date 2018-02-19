9be9d50185569183118c0d311db26f7e0ab92676
usuario <usuario@161.67.196.139>
2018-02-19 14:10:30 +0100

    123

--- B/T_PrintID.java
+++ B/T_PrintID.java
@@ -8,6 +8,24 @@ package concurrent_assignment1.B;
  * @author yournamehere
  */
 
-public class T_PrintID {
+public class T_PrintID extends Thread{
+
+    int ID;
+    
+    public T_PrintID(int a){
+        ID=a;
+    }
+    
+    public void run(){
+        for (int i =0; i<10; i++){
+            System.out.println(ID);
+        }
+    }
+    public static void main (String argsString[]){
+        
+        (new T_PrintID(11)).start();
+        
+    }
+
 
 }

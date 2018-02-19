c3a88f5160623553ecffbd5dd6f5ceaa5ac44a73
usuario <usuario@161.67.196.139>
2018-02-19 13:49:32 +0100

    123

--- A/Reference.java
+++ A/Reference.java
@@ -1,15 +1,22 @@
 package concurrent_assignment1.A;
 
+import static java.lang.Thread.sleep;
+
 /**
  * Get the reference of the main thread */
 
 public class Reference{
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread h = Thread.currentThread ();  
+        System.out.println("ID:"+h.getName());
+        h.setName("mainT");
+        System.out.println("ID:"+h.getName());
+
         
     for (int i=1; i<100; i=i+2){
             System.out.println(i);
+            sleep(2000);
         }
     }
 }

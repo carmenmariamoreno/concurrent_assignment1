911bef4134917d3765663306ff56415e864322d3
usuario <usuario@161.67.196.139>
2018-02-26 12:46:47 +0100

    123

--- C/Numbers.java
+++ C/Numbers.java
@@ -8,6 +8,18 @@ package concurrent_assignment1.C;
  *
  */
 
-public class Numbers {
-
+public class Numbers extends Thread{
+    int id;
+    
+        @Override
+    public void run(){
+       for (int i = 0; i<5; i++){
+       (new Thread (new R_PrintID (i))).start();
+       }
+    }
+     
+    public static void main (String args[]){
+       
+       (new Numbers()).start();
+    }
 }

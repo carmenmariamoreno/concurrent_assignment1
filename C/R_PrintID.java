a440a07a07ac129c3556ff5f02c0bed75e49e4ae
usuario <usuario@161.67.196.140>
2018-02-26 13:08:47 +0100

    a

--- C/R_PrintID.java
+++ C/R_PrintID.java
@@ -8,6 +8,42 @@ package concurrent_assignment1.C;
  *
  */
 
-public class R_PrintID {
+import static java.lang.Thread.sleep;
+import java.util.Random;
 
+
+public class R_PrintID implements Runnable {
+
+    int ID;
+    Random nrandom = new Random(System.currentTimeMillis());
+    
+    
+    public R_PrintID(){
+        ID=10;
+    }
+    
+    public R_PrintID(int n){
+        ID=n;
+    }
+    
+    public void run(){
+        
+        for (int i =0; i<10; i++){
+            //System.out.println(ID);
+            
+            try{
+                Thread.sleep(nrandom.nextInt(1000));
+                System.out.println(ID);
+                
+            } catch(InterruptedException ex){
+                System.out.println("Interrupted Exception " +ex);
+            }
+        }
+    }
+    public static void main (String argsString[]){
+        
+        (new Thread(new R_PrintID())).start();
+       
+        
+    }
 }
